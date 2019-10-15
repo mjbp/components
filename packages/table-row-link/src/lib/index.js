cbe183ae27be667d672f102cf379298c5d315a1f
Mick Perkins <mick.perkins@stormid.com>
2019-10-15 15:29:38 +0100

    Removes superfluous table row code

--- packages/table-row-link/src/lib/index.js
+++ packages/table-row-link/src/lib/index.js
@@ -1,13 +1,3 @@
-import { dispatchSyntheticEvent } from './utils';
-
-const toggle = () => {
-    if(!state.node.checked) return;
-    state.groupNodes.forEach(node => {
-        node.checked = state.node.checked ? false : node.checked;
-        dispatchSyntheticEvent(node, 'change');
-    });
-};
-
 const findParentRow = node => {
     let row = false,
         parent = node.parentNode;
@@ -31,27 +21,6 @@ export default (node, settings) => {
     state.row.addEventListener('focusout', e => {
         state.row.classList.remove(settings.activeClassName);
     });
-
-
-
-
-
-    // const groupNodes = [].slice.call(document.querySelectorAll(`[${DATA_ATTRIBUTES.GROUP_NAME}="${node.getAttribute(DATA_ATTRIBUTES.SELECTOR)}"]`));
-    // state = Object.assign({}, state, {
-    //     node,
-    //     settings,
-    //     groupNodes
-    // });
-    // TRIGGER_EVENTS.forEach(ev => {
-    //     state.node.addEventListener(ev, e => {
-    //         if(!e.keyCode || (node.nodeName.toLowerCase() !== 'input' && !!~KEY_CODES.indexOf(e.keyCode))) toggle();
-    //     });
-    //     state.groupNodes.forEach(groupNode => {
-    //         groupNode.addEventListener(ev, e => {
-    //             if((!e.keyCode || (node.nodeName.toLowerCase() !== 'input' && !!~KEY_CODES.indexOf(e.keyCode))) && node.checked) state.node.checked = false;
-    //         });
-    //     });
-    // });
 
     return state;
 };
